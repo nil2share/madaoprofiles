f637d7ec224013d3feaa685a97ff25e7f7b55da1
Nil2 Share <Nil2Share@gmail.com>
2020-11-03 09:40:50 -0500

    new yellow script gathering profiles

---
+++ GatherScheduleProfiles/Nil-Yellow-Bot-71-80.lua
@@ -0,0 +1,215 @@
+local tbl = 
+{
+	filename = "Nil-Yellow-Bot-71-80",
+	glist = 
+	{
+		
+		{
+			HQamount = 0,
+			amount = 100,
+			col_ga_crystal = true,
+			collect = true,
+			cordial = false,
+			gearset = 0,
+			id = 27819,
+			job = 17,
+			markerid = 8131210,
+			markername = "Mapname:[Lakeland] Mapid: 813 Normal Level:80 X:12 Y:10",
+			notinlistcaseid = 0,
+			okgp = 600,
+			skillname = "Collective_Default.lua",
+			skillnum = 2,
+			skip = false,
+			tasktype = "gather",
+			timerValue = 1048787,
+		},
+		
+		{
+			HQamount = 0,
+			amount = 100,
+			collect = true,
+			cordial = true,
+			id = 27821,
+			job = 17,
+			markerid = 8151630,
+			markername = "Mapname:[Amh Araeng] Mapid: 815 Normal Level:80 X:16 Y:30",
+			notinlistcaseid = 0,
+			okgp = 600,
+			skillname = "Collective_Default.lua",
+			skip = false,
+			tasktype = "gather",
+			timerValue = 909292,
+		},
+	},
+	orders = 
+	{
+	},
+	unspoil = 
+	{
+		
+		{
+			HQamount = 0,
+			amount = 0,
+			collect = true,
+			cordial = false,
+			expansion = "Shadow",
+			folklore = false,
+			gearset = 0,
+			hold = false,
+			id = 27833,
+			job = 17,
+			mapid = 816,
+			markerid = 8162436,
+			move_after_timer = false,
+			name = "Broad Beans",
+			okgp = 600,
+			reserve = false,
+			skillname = "Collective_Default.lua",
+			skillnum = 2,
+			skip = false,
+			slot = 2,
+			timeChecks = 
+			{
+				true, 
+				[0] = true,
+				[12] = true,
+				[13] = true,
+			},
+			timeoks = 
+			{
+				0,
+				1,
+				12,
+				13,
+			},
+			x = 24,
+			y = 36,
+		},
+		
+		{
+			HQamount = 0,
+			amount = 0,
+			collect = true,
+			cordial = false,
+			expansion = "Shadow",
+			folklore = false,
+			gearset = 0,
+			hold = false,
+			id = 27822,
+			job = 17,
+			mapid = 815,
+			markerid = 8151916,
+			move_after_timer = false,
+			name = "Russet Popoto",
+			okgp = 600,
+			reserve = false,
+			skillname = "Collective_Default.lua",
+			skillnum = 2,
+			skip = false,
+			slot = 7,
+			timeChecks = 
+			{
+				[8] = true,
+				[9] = true,
+				[20] = true,
+				[21] = true,
+			},
+			timeoks = 
+			{
+				8,
+				9,
+				20,
+				21,
+			},
+			x = 19,
+			y = 16,
+		},
+	},
+	unspoilTimeline = 
+	{
+		
+		{
+			27833,
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+			27822,
+		},
+		
+		{
+			27822,
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+			27833,
+		},
+		
+		{
+			27833,
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+			27822,
+		},
+		
+		{
+			27822,
+		},
+		
+		{
+		},
+		
+		{
+		}, 
+		[0] = 
+		{
+			27833,
+		},
+	},
+}
+
+
+
+return tbl
---
+++ GatherScheduleProfiles/Nil-Yellow-Min-71-80.lua
@@ -0,0 +1,241 @@
+local tbl = 
+{
+	eph = 
+	{
+	},
+	ephs = 
+	{
+		[0] = 
+		{
+		},
+		[4] = 
+		{
+		},
+		[8] = 
+		{
+		},
+		[12] = 
+		{
+		},
+		[16] = 
+		{
+		},
+		[20] = 
+		{
+		},
+	},
+	filename = "Nil-Yellow-Min-71-80",
+	glist = 
+	{
+		
+		{
+			HQamount = 0,
+			amount = 100,
+			col_ga_crystal = true,
+			collect = true,
+			cordial = false,
+			gearset = 0,
+			id = 27782,
+			job = 16,
+			markerid = 8133514,
+			markername = "Mapname:[Lakeland] Mapid: 813 Normal Level:80 X:35 Y:14",
+			notinlistcaseid = 0,
+			okgp = 600,
+			skillname = "Collective_Default.lua",
+			skillnum = 2,
+			skip = false,
+			tasktype = "gather",
+			timerValue = 1182312,
+		},
+		
+		{
+			HQamount = 0,
+			amount = 100,
+			col_ga_crystal = false,
+			collect = true,
+			cordial = false,
+			id = 27776,
+			job = 16,
+			markerid = 8172529,
+			markername = "Mapname:[The Rak'tika] Mapid: 817 Normal Level:80 X:25 Y:29",
+			notinlistcaseid = 0,
+			okgp = 600,
+			skillname = "Collective_Default.lua",
+			skillnum = 2,
+			skip = false,
+			tasktype = "gather",
+			timerValue = 1035163,
+		},
+	},
+	orders = 
+	{
+	},
+	unspoil = 
+	{
+		
+		{
+			HQamount = 0,
+			amount = 0,
+			collect = true,
+			cordial = false,
+			expansion = "Shadow",
+			folklore = false,
+			gearset = 0,
+			hold = false,
+			id = 27727,
+			job = 16,
+			mapid = 816,
+			markerid = 8162613,
+			move_after_timer = false,
+			name = "Raw Diaspore",
+			okgp = 600,
+			reserve = false,
+			skillname = "Leveling.lua",
+			skillnum = 8,
+			skip = false,
+			slot = 6,
+			timeChecks = 
+			{
+				[6] = true,
+				[7] = true,
+				[18] = true,
+				[19] = true,
+			},
+			timeoks = 
+			{
+				6,
+				7,
+				18,
+				19,
+			},
+			x = 26,
+			y = 13,
+		},
+		
+		{
+			HQamount = 0,
+			amount = 0,
+			collect = true,
+			cordial = false,
+			expansion = "Shadow",
+			folklore = false,
+			gearset = 0,
+			hold = false,
+			id = 27729,
+			job = 16,
+			mapid = 815,
+			markerid = 8152029,
+			move_after_timer = false,
+			name = "Raw Triplite",
+			okgp = 600,
+			reserve = false,
+			skillname = "Collective_Default.lua",
+			skillnum = 2,
+			skip = false,
+			slot = 7,
+			timeChecks = 
+			{
+				true, 
+				[0] = true,
+				[12] = true,
+				[13] = true,
+			},
+			timeoks = 
+			{
+				0,
+				1,
+				12,
+				13,
+			},
+			x = 20,
+			y = 29,
+		},
+	},
+	unspoilTimeline = 
+	{
+		
+		{
+			27729,
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+			27727,
+		},
+		
+		{
+			27727,
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+			27729,
+		},
+		
+		{
+			27729,
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+			27727,
+		},
+		
+		{
+			27727,
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		},
+		
+		{
+		}, 
+		[0] = 
+		{
+			27729,
+		},
+	},
+}
+
+
+
+return tbl
